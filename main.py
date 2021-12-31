0d901756654887d6b161af734c236555ee044db3
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-31 23:33:03 +0000

    Python 'Dynamic typed' language.

--- main.py
+++ main.py
@@ -16,13 +16,15 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def timer_count():
-    count_down(5 * 60)
+    count_down(.1 * 60)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
-    count_sec = count % 60
+    count_sec = int(count % 60)
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
     canvas.itemconfig(canvas_timer, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
