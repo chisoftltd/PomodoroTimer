02577d36cf5b4ad2705618c926564bc350a2876e
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-31 22:53:52 +0000

    Adding image and text to Tkinter using Canvas

--- main.py
+++ main.py
@@ -18,27 +18,27 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-
 window = Tk()
-window.minsize(width=600, height=400)
 window.title("POMODORO")
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
-title_label = Label(text="TIMER", font=(FONT_NAME, 25), fg=GREEN)
-title_label.place(x=250, y=15)
+canvas = Canvas(width=200, height=250, bg=YELLOW, highlightthickness=0)
+# title_label = Label(text="TIMER", font=(FONT_NAME, 25), fg=GREEN)
+# title_label.place(x=50, y=5)
 
 apple_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=apple_image)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
-timer_label = Label(text="00:00", fg=GREEN, font=(FONT_NAME, 10))
-timer_label.place(x=280, y=170)
-
-start_button = Button(text="Start", font=(FONT_NAME, 15))
-start_button.place(x=100, y=300)
-
-stop_button = Button(text="Stop", font=(FONT_NAME, 15))
-stop_button.place(x=450, y=300)
+# timer_label = Label(text="00:00", fg=GREEN, font=(FONT_NAME, 10))
+# timer_label.place(x=180, y=170)
+#
+# start_button = Button(text="Start", font=(FONT_NAME, 15))
+# start_button.place(x=100, y=300)
+#
+# stop_button = Button(text="Stop", font=(FONT_NAME, 15))
+# stop_button.place(x=350, y=300)
 
 
 window.mainloop()
