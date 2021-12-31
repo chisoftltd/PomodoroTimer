fc3a9bbb4e3f984bb350530c71630aecb6242fb4
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-31 23:25:23 +0000

    Adding window.after and math function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from PIL import Image, ImageTk
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,11 +11,22 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def timer_count():
+    count_down(5 * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(canvas_timer, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -28,17 +40,17 @@ title_label.grid(column=1, row=0)
 
 apple_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=apple_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas_timer = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 check_label = Label(text="✔", font=(FONT_NAME, 25), fg=GREEN, bg=YELLOW)
 check_label.grid(column=1, row=3)
 
-start_button = Button(text="Start", font=(FONT_NAME, 10), highlightthickness=0)
+
+start_button = Button(text="Start", font=(FONT_NAME, 10), highlightthickness=0, command=timer_count)
 start_button.grid(column=0, row=3)
 
 stop_button = Button(text="Restart", font=(FONT_NAME, 10), highlightthickness=0)
 stop_button.grid(column=2, row=3)
-
 
 window.mainloop()
