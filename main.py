e27da342d8d8c499d7cd0be0045b7ca2b07b07d9
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-02 17:04:09 +0000

    COUNTDOWN MECHANISM & TIMER MECHANISM

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+import time
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,24 +11,44 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def timer_count():
-    count_down(.1 * 60)
+    global reps
+    reps += 1
+
+    work_sec = 1 * 60 # WORK_MIN * 60
+    short_break_sec = 1 * 60 # SHORT_BREAK_MIN * 60
+    long_break_sec = 1 * 60 # LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Long Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Short Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
+    if count_min < 10:
+        count_min = f"0{count_min}"
     count_sec = int(count % 60)
     if count_sec < 10:
         count_sec = f"0{count_sec}"
     canvas.itemconfig(canvas_timer, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        timer_count()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -47,7 +68,6 @@ canvas.grid(column=1, row=1)
 
 check_label = Label(text="✔", font=(FONT_NAME, 25), fg=GREEN, bg=YELLOW)
 check_label.grid(column=1, row=3)
-
 
 start_button = Button(text="Start", font=(FONT_NAME, 10), highlightthickness=0, command=timer_count)
 start_button.grid(column=0, row=3)
